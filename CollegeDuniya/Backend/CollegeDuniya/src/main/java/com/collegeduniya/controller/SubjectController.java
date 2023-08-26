39e7f422514ffdd918cdb43d403f3609055af9f9
Siraj <sirajmohammad286@gmail.com>
2023-08-27 05:01:52 +0530

    crossorigin and mapping url updated

--- CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/controller/SubjectController.java
+++ CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/controller/SubjectController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,9 @@ import com.collegeduniya.dto.ApiResponse;
 import com.collegeduniya.dto.SubjectDto;
 import com.collegeduniya.service.SubjectService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/subjects")
+@RequestMapping("/api/v1/subjects")
 public class SubjectController {
 
 	@Autowired
