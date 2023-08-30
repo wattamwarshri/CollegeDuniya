7561879c764c5568e8265a3f3caacc7f905302b2
Varun-dubey <myselfvarundubey@gmail.com>
2023-08-30 18:21:29 +0530

    updated UserServiceImpl, UserRepository

--- CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/entities/Professor.java
+++ CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/entities/Professor.java
@@ -1,7 +1,6 @@
 package com.collegeduniya.entities;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
 import lombok.AllArgsConstructor;
