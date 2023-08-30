c898b2dc426ebdbc37e309798ea8520451c2dc38
Siraj <sirajmohammad286@gmail.com>
2023-08-30 09:53:04 +0530

    Added User.java

---
+++ CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/entities/User.java
@@ -0,0 +1,49 @@
+package com.collegeduniya.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//import org.springframework.context.annotation.Role;
+
+//import com.collegeduniya.entities.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	@Column(length = 20,unique = true)
+    private String name;
+	
+	@Column(length = 20,unique = true)
+	private String username;
+	
+	@Column(length = 20,unique=true)
+	private String password;
+	
+	@Column(length = 100)
+	private String address;
+	
+	@Column(length = 10,unique=true)
+    private String contact;
+	
+	//@Enumerated(EnumType.STRING)
+	//private Role role;
+	
+}
