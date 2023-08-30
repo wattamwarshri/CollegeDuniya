1308a9914ed08fb291fcd2c3e714ac81f58f31a9
Siraj <sirajmohammad286@gmail.com>
2023-08-30 09:58:02 +0530

    Added UserRepository.java

---
+++ CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.collegeduniya.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import com.collegeduniya.entities.Subject;
+import com.collegeduniya.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByUsername(String username);
+
+}
