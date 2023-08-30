083e926dd17af3d4d43ac4d4b9c652331b24d010
ShRi <wattamwarshri1234@gmail.com>
2023-08-30 19:03:59 +0530

    React: Header Component updated

--- CollegeDuniya/Frontend/college-duniya/src/component/HeaderComponent.jsx
+++ CollegeDuniya/Frontend/college-duniya/src/component/HeaderComponent.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-
+import collegeLogo from '../img/fav-icon-college.jpg'
 const HeaderComponent = () => {
   return (
     <div>
       <header className="App-header">
                     <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
                         <div className="container-fluid">
-                        {/* <img src="%PUBLIC_URL%/fav-icon1.png" alt="not found" /> */}
+                        <img src={collegeLogo}  alt="not found" style={{"height":40 +"px", "width" : 40 + "px", "marginRight": 10 + "px"}}/>
                         
-                            <a className="navbar-brand" href="/">CollegeDuniya</a>
+                            <a className="navbar-brand">CollegeDuniya</a>
                            
                             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                                 <div className="navbar-nav">
-                                    <a className="nav-link active" aria-current="page" href="/students">Students</a>
-                                    <a className="nav-link" href="/departments">Departments</a>
-                                    <a className="nav-link" href="courses">Courses</a>
-                                    <a className="nav-link" href="/professors">Professors</a>
-                                    <a className="nav-link" href="/subjects">Subjects</a>
-                                    <a className="nav-link" href="/exams">Exams</a>
+                                   
                                     
                                 </div>
                             </div>
