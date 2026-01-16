443fd61c490615d611ff35708b626d15b22bbe8b
shrinivas.wattamwar <wattamwarshri1234@gmail.com>
2026-01-17 00:41:31 +0530

    test: restructuring

--- CollegeDuniya/Backend/CollegeDuniya/src/main/java/com/collegeduniya/custom_exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.collegeduniya.custom_exceptions;
-
-@SuppressWarnings("serial")
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String mesg) {
-		super(mesg);
-	}
-}
